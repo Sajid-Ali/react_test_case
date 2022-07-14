d0a06dfdc9e3355b4fddf2d25794fe382d4ebeaa
Sajid Ali <sajid@aiondigital.com>
2022-07-14 14:33:44 +0500

    added basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function App({username, password}) {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (event) => {
+    // Prevent page reload
+    event.preventDefault();
+    console.log(event, '-=-=-=-=-=-=-')
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div className="form" data-testid="login-form">
+      <form>
+        <div className="input-container">
+          <label>Username </label>
+          <input id="username" type="text" name="username" required />
+          {renderErrorMessage("username")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input data-testid="password" type="password" name="password" required />
+          {renderErrorMessage("password")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-form">
+      <div className="title" data-testid="title">Sign In</div>
+      {renderForm}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,70 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/no-node-access */
+/* eslint-disable testing-library/no-unnecessary-act */
+import '@testing-library/jest-dom'
+import React from "react";
+import { render, screen, cleanup } from '@testing-library/react';
+import { unmountComponentAtNode } from "react-dom";
+import { act } from 'react-dom/test-utils';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+  
+  cleanup();
+});
+
+describe("Login Component", () => {
+  // Test 1
+  test('login component', () => {
+    render(<App />);
+    const element = screen.getByText(/sign in/i);
+    expect(element).toBeInTheDocument();
+  });
+
+  // Test 2
+  test("Rendering", () => {
+    render(<App />);
+    const title = screen.getByTestId("title");
+    expect(title).toBeInTheDocument();
+  })
+
+  // Test 3
+  test("Header Text", () => {
+    render(<App />);
+    const title = screen.getByTestId("title");
+    expect(title).toHaveTextContent("Sign In");
+  })
+});
+
+it("data fetching", async () => {
+  const credential = {
+    username: "testing",
+    password: "testing",
+  };
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(credential)
+    })
+  );
+
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<App id="123" />, container);
+  });
+
+  expect(container.querySelector("#username").textContent).toBe(credential.username);
+  expect(container.querySelector("#password").textContent).toBe(credential.password);
+
+  // remove the mock to ensure tests are completely isolated
+  global.fetch.mockRestore();
 });
